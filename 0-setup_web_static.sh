913803cbf7e87085460b72fc037518d025942557
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-31 16:31:58 +0000

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
 # Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-	sudo apt-get update
-	sudo apt-get install -y nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo touch /data/web_static/releases/test/index.html
+# Create /data/ if it doesn't exist
+sudo mkdir -p /data
+
+# Create /data/web_static/ if it doesn't exist
+sudo mkdir -p /data/web_static
+
+# Create /data/web_static/releases/ if it doesn't exist
+sudo mkdir -p /data/web_static/releases
+
+# Create /data/web_static/shared/ if it doesn't exist
+sudo mkdir -p /data/web_static/shared
+
+# Create /data/web_static/releases/test/ if it doesn't exist
+sudo mkdir -p /data/web_static/releases/test
+
+# Create a fake HTML file with simple content
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
