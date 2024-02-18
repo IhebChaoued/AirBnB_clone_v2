27f71f33bd6210269bf83fc7d1653381b5d2a75c
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-18 17:27:32 +0000

    Add Route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Flask App with Additional Route """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greet():
+    """ Route for the main index """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """ Route for the '/hbnb' endpoint """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """ Route for the '/c/<text>' endpoint """
+    text = text.replace("_", " ")
+    return "C " + f"{text}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_framework(text):
+    """ Route for the '/python' and '/python/<text>' endpoints """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    # Run the Flask app on specified host and port
+    app.run(host="0.0.0.0", port=5000)
