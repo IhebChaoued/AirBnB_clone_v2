e849b0ca3267984b9d16646fe558277d14d8263c
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-18 18:17:53 +0000

    state

--- models/state.py
+++ models/state.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 """Defines the State class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database"""
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    """ Represents a state for a MySQL database """
 
-    cities = relationship("City",  backref="state", cascade="delete")
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
+            """ Cities """
+            from models import storage
+            citiesList = []
+            citiesAll = storage.all(City)
+            for city in citiesAll.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    citiesList.append(city)
+            return citiesList
