36e96b9403b7812e28412f36d512a00eab2c13f9
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-18 18:25:21 +0000

    list states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts the Flask app"""
+""" module doc """
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -8,18 +8,59 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def doc """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def doc """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        p = 'even'
+    else:
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """List states"""
+    """ def doc """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(error):
-    """Teardown context"""
+def close(error):
+    """ def doc """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
